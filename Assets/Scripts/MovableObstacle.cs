b1db84a5af8e2840f46a72609a956afd6b5cd405
Pedro Bandeira <pedro.bandeira@tap4mobile.com.br>
2018-01-28 11:52:27 -0400

    Adição das artes e plataformas móveis

---
+++ Assets/Scripts/MovableObstacle.cs
@@ -0,0 +1,30 @@
+﻿using UnityEngine;
+
+public class MovableObstacle : MonoBehaviour
+{
+    private float useSpeed;
+    public float directionSpeed = 9.0f;
+    float origX;
+    public float distance = 10.0f;
+
+    // Use this for initialization
+    void Start()
+    {
+        origX = transform.position.x;
+        useSpeed = -directionSpeed;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (origX - transform.position.x > distance)
+        {
+            useSpeed = directionSpeed; //flip direction
+        }
+        else if (origX - transform.position.x < -distance)
+        {
+            useSpeed = -directionSpeed; //flip direction
+        }
+        transform.Translate(useSpeed * Time.deltaTime, 0, 0);
+    }
+}
